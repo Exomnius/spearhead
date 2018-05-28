5bd59b4ab1e53112d390a52e59da21127977d9c3
Cornel Janssen <cornel.janssen@elision.eu>
2018-05-28 21:30:22 +0200

    route splitting done

--- packages/web/src/pages/Contact/LoadableContact.jsx
+++ packages/web/src/pages/Contact/LoadableContact.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 
+const Loading = () => (
+  <div>Loading...</div>
+);
+
 const LoadableContact = Loadable({
-  loader: () => import(/* webpackChunkName: 'contact' */ './Contact'),
-  loading() {
-    return <div>Loading...</div>;
-  }
+  loader: () => import(/* webpackChunkName: 'contact' */ './Contact').then(module => module.default),
+  loading: Loading
 });
 
 
